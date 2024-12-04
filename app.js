1c70b37575456e4fdaa52a727157dad06cd32ce5
Rokas <opt.imistas@inbox.lt>
2024-12-04 12:00:19 +0200

    ver1

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const renderPrioritySelectorData = _ => {
     selector.innerHTML = html;
 };
 
-
+let TASKS = [];
 
 const init = _ => {
     const addButton = document.querySelector('[data-task-add]');
@@ -28,9 +28,32 @@ const init = _ => {
     renderPrioritySelectorData();
 };
 
-// const renderAddTask = _ => {
+const renderAddTask = _ => {
+    
+    const dataTaskList = document.querySelector('[data-task-list]');
+    const id = getRandomIntInclusive(10000000, 99999999);
+    const task = document.querySelector('[data-task-input]');
+    const priority = document.querySelector('[data-task-priority]');
+    const done = false
 
-// };
+    let li = `<li data-id="${id}">
+                    <div class="task" data-task-view-priority="${priority}">
+                        <div class="content">${task.value}</div>
+                        <div class="actions">
+                            <button class="done" ${done ? '' : 'disabled'} data-task-done>Atlikta</button>
+                            <button class="delete" data-task-delete>Pasalinti</button>
+                        </div>
+ 
+                    </div>
+                </li>`;
+
+    dataTaskList.innerHTML += li;
+    console.log(dataTaskList.innerHTML);
+
+    task.value = '';
+    priority.value = '1';
+    
+};
 
 init();
 
